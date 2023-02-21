b68d19be91f3365f38f4c0a4d41c362e359419c7
craftsmandigital <hackjack@tutanota.com>
2023-02-21 09:49:20 +0100

    Added a new state with the Stately tool

--- test.js
+++ test.js
@@ -1,42 +1,78 @@
+// added new state Soaping to the machine with the stately tool
+// https://stately.ai/registry/editor/9a2446c2-b6b3-4deb-98cb-a818872efe48?machineId=f959d9e0-4959-458e-9ebf-725e9a55d2ac
+
+
 
 const { Machine, interpret } = require('xstate');
 
 const handWashMachine1 = Machine({
-  id: 'handwash',
-  initial: 'unwashed',
+  id: "handwash",
+  initial: "unwashed",
   states: {
     unwashed: {
-      entry: 'logEntry',
+      entry: "logEntry",
       on: {
-        START_WASH: 'washing'
-      }
+        START_WASH: {
+          target: "washing",
+        },
+      },
     },
     washing: {
-
-      entry: 'logEntry',
+      entry: "logEntry",
       on: {
-        STOP_WASH: 'washed'
-      }
+        STOP_WASH: {
+          target: "washed",
+        },
+        START_SOAPING: {
+          target: "Soaping",
+          // cond: "dirty",
+        },
+      },
     },
     washed: {
-
-      entry: 'logEntry',
-      type: 'final'
-    }
-  }
+      entry: "logEntry",
+      type: "final",
+    },
+    Soaping: {
+      entry: "logEntry",
+      on: {
+        STOP_SOAPING: {
+          target: "washing",
+        },
+      },
+    },
+  },
+  // context: {},
+  // predictableActionArguments: true,
+  // preserveActionOrder: true,
 },
-{
-  actions: {
-    logEntry: (context, event, meta) => {
-      console.log(`Entering state: ${meta.state.value}`);
+  {
+    actions: {
+      logEntry: (context, event, meta) => {
+        console.log(`Entering state: ${meta.state.value}`);
+      }
     }
-  }
-});
+  });
 
 const washService = interpret(handWashMachine1);
 washService.start();
-washService.send({type: 'STOP_WASH'});
-washService.send({type: 'START_WASH'});
-washService.send({type: 'STOP_WASH'});
-washService.send({type: 'STOP_WASH'});
+washService.send({ type: 'START_WASH' });
+washService.send({ type: 'START_WASH' }); // wil not trigger
+washService.send({ type: 'START_WASH' }); // wil not trigger
+washService.send({ type: 'START_WASH' }); // wil not trigger
+washService.send({ type: 'START_WASH' }); // wil not trigger
+washService.send({ type: 'START_WASH' }); // wil not trigger
+washService.send({ type: 'START_WASH' }); // wil not trigger
+washService.send({ type: 'START_SOAPING' });
+washService.send({ type: 'STOP_SOAPING' });
+washService.send({ type: 'START_SOAPING' });
+washService.send({ type: 'STOP_SOAPING' });
+washService.send({ type: 'STOP_WASH' });
 // console.log(washService.currentState);
+
+
+console.log("Start antother simpler path");
+washService.start();
+washService.send({ type: 'START_WASH' });
+
+washService.send({ type: 'STOP_WASH' });
